c5cbc86f81861476631af51a6fe2a87f783dd583
Justin, Chang-Qi Zhang <justin840727@gmail.com>
2018-09-26 17:12:21 +0800

    first commit hw2

---
+++ hw2-1_grey_level_resolution/include/grey_level_resolution.hpp
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
---
+++ hw2-1_grey_level_resolution/src/grey_level_resolution.cpp
@@ -0,0 +1,11 @@
+#include "grey_level_resolution.hpp"
+
+int main(int argc, char **argv)
+{
+  // int width = 512;
+  // int height = 512;
+  // int size = width * height;
+  // char input_file[] = "../images/lena_512.raw";      // Input Image name
+  // char output_file[] = "../images/lena_512_draw.png"; // Output Image name
+  return 0;
+}
