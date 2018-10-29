d3de06866749b5de6f822fb3bb40b67d621cf4d9
Justin, Chang-Qi Zhang <justin840727@gmail.com>
2018-10-29 23:33:17 +0800

    add sharped image

--- hw4-2_image_sharpning/src/subtraction_operation.cpp
+++ hw4-2_image_sharpning/src/subtraction_operation.cpp
@@ -132,6 +132,23 @@ cv::Mat sharpingImg(cv::Mat &src, cv::Mat &src_lpf, double c)
   return out_img;
 }
 
+cv::Mat sharpingImg2(cv::Mat &src, cv::Mat &src_lpf, double c)
+{
+  cv::Mat out_img(src.rows, src.cols, CV_32SC1);
+  for (int i = 0; i < src.rows; i++)
+  {
+    for (int j = 0; j < src.cols; j++)
+    {
+      int px = 0;
+      px = src.at<uint8_t>(i, j) + c*(src.at<uint8_t>(i, j) - src_lpf.at<uint8_t>(i, j));
+      px = std::max(0, px);
+      // px = std::min(255, px);
+      out_img.at<int>(i, j) = px;
+    }
+  }
+  return out_img;
+}
+
 cv::Mat Normalize(cv::Mat &src)
 {
   cv::Mat nor_mat(src.rows, src.cols, CV_8UC1);
@@ -160,6 +177,34 @@ cv::Mat Normalize(cv::Mat &src)
   return nor_mat;
 }
 
+cv::Mat NormalizeInt(cv::Mat &src)
+{
+  cv::Mat nor_mat(src.rows, src.cols, CV_8UC1);
+  std::vector<int> px_list;
+  for (int i = 0; i < src.rows; i++)
+  {
+    for (int j = 0; j < src.cols; j++)
+    {
+      px_list.push_back(src.at<int>(i, j));
+    }
+  }
+  int max_px = *max_element(px_list.begin(), px_list.end());
+  int min_px = *min_element(px_list.begin(), px_list.end());
+  // std::cout<<"Max value: "<< max_px << std::endl;
+  // std::cout<<"Min value: "<< min_px << std::endl;
+  double scale = 255.0 / (max_px - min_px);
+  // std::cout << scale << std::endl;
+  for (int i = 0; i < src.rows; i++)
+  {
+    for (int j = 0; j < src.cols; j++)
+    {
+       nor_mat.at<uint8_t>(i, j) = src.at<int>(i, j) * scale;
+    }
+  }
+  // std::cout << nor_mat << std::endl;
+  return nor_mat;
+}
+
 int main(int argc, char **argv)
 {
   cv::Mat src(512, 512, CV_8UC1);
@@ -180,17 +225,34 @@ int main(int argc, char **argv)
   cv::Mat sharped_nor_img_1 = Normalize(sharped_img_1);
   cv::Mat sharped_img_n_03 = sharpingImg(src_n, box_n_img, 0.3);
   cv::Mat sharped_nor_n_img_03 = Normalize(sharped_img_n_03);
+  // sharped image
+  cv::Mat sharped2_03 = sharpingImg2(src, box_img, 0.3);
+  cv::Mat sharped2_05 = sharpingImg2(src, box_img, 0.5);
+  cv::Mat sharped2_08 = sharpingImg2(src, box_img, 0.8);
+  cv::Mat sharped2_1 = sharpingImg2(src, box_img, 1);
+  cv::Mat sharped2_n_08 = sharpingImg2(src_n, box_n_img, 0.8);
+  cv::Mat sharped2_03_nor = NormalizeInt(sharped2_03);
+  cv::Mat sharped2_05_nor = NormalizeInt(sharped2_05);
+  cv::Mat sharped2_08_nor = NormalizeInt(sharped2_08);
+  cv::Mat sharped2_1_nor = NormalizeInt(sharped2_1);
+  cv::Mat sharped2_n_08_nor = NormalizeInt(sharped2_n_08);
   showImage("src", src);
-  showImage("sharped 0.3", sharped_nor_img_03);
-  showImage("sharped 0.5", sharped_nor_img_05);
-  showImage("sharped 0.8", sharped_nor_img_08);
-  showImage("sharped 1", sharped_nor_img_1);
-  showImage("sharped noise", sharped_nor_n_img_03);
+  showImage("sharped 0.3", sharped2_03_nor);
+  showImage("sharped 0.5", sharped2_05_nor);
+  showImage("sharped 0.8", sharped2_08_nor);
+  showImage("sharped 1", sharped2_1_nor);
+  showImage("sharped noise", sharped2_n_08_nor);
   saveImage(sharped_nor_img_03, "../result_img/", "sharped_img_03");
   saveImage(sharped_nor_img_05, "../result_img/", "sharped_img_05");
   saveImage(sharped_nor_img_08, "../result_img/", "sharped_img_08");
   saveImage(sharped_nor_img_1, "../result_img/", "sharped_img_1");
   saveImage(sharped_nor_n_img_03, "../result_img/", "sharped_n_img_03");
+  
+  saveImage(sharped2_03_nor, "../result_img/", "sharped2_03");
+  saveImage(sharped2_05_nor, "../result_img/", "sharped2_05");
+  saveImage(sharped2_08_nor, "../result_img/", "sharped2_08");
+  saveImage(sharped2_1_nor, "../result_img/", "sharped2_1");
+  saveImage(sharped2_n_08_nor, "../result_img/", "sharped2_n_08");
   cv::waitKey(0);
   return 0;
 }
