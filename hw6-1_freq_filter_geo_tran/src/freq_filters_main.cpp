8ecaf83472262b5721eec2d3c1601060cae3f949
Justin, Chang-Qi Zhang <justin840727@gmail.com>
2018-11-15 17:40:50 +0800

    ideal, gaussian and butterworth filters

--- hw6-1_freq_filter_geo_tran/src/freq_filters_main.cpp
+++ hw6-1_freq_filter_geo_tran/src/freq_filters_main.cpp
@@ -2,7 +2,6 @@
 #include "cv_dft_2d.hpp"
 #include "freq_filters.hpp"
 
-
 int main(int argc, char **argv)
 {
   cv::Mat fox_src = loadRawFile("../images/fox.raw", 380, 284);
@@ -14,7 +13,7 @@ int main(int argc, char **argv)
   showImage("fox_src",fox_src);
   showImage("fox_sp",fox_sp);
 
-  // ideal filter
+  // Ideal filter
   cv::Mat fox_ilpf_15(fox_dft_i.rows, fox_dft_i.cols, CV_32FC2);
   cv::Mat ilpf_15 =  fdf::idealLpf(fox_dft.getComplex(), 15);
   fdf::applyFilter(fox_dft_i, ilpf_15, fox_ilpf_15);
@@ -54,11 +53,81 @@ int main(int argc, char **argv)
   saveImage(glpf_50, "../result_img/1.a/", "gaussion_lpf_50_sp");
   saveImage(fox_g_idft_img_50, "../result_img/1.a/", "gaussion_lpf_50_idft");
 
-  showImage("fox_g_idft_15",fox_g_idft_img_15);
-  showImage("fox_g_dft_filter_sp_15",fox_g_lpf_15_sp);
-  showImage("fox_g_idft_50",fox_g_idft_img_50);
-  showImage("fox_g_dft_filter_sp_50",fox_g_ilpf_50_sp);
+  // Butterworth LPF D0 = 15 n = 1
+  cv::Mat fox_b_lpf_15_n1(fox_dft_i.rows, fox_dft_i.cols, CV_32FC2);
+  cv::Mat blpf_15_n1 =  fdf::ButterworthLpf(fox_dft.getComplex(), 15, 1);
+  fdf::applyFilter(fox_dft_i, blpf_15_n1, fox_b_lpf_15_n1);
+  cv::Mat fox_b_lpf_15_n1_sp = CvDft2d::getSpectrumImg(fox_b_lpf_15_n1);
+  CvIDft2d fox_b_idft_15_n1(fox_b_lpf_15_n1);
+  cv::Mat fox_b_idft_15_n1_img =  fox_b_idft_15_n1.getInvImg();
+  cv::normalize(blpf_15_n1, blpf_15_n1, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  saveImage(blpf_15_n1, "../result_img/1.b/", "butterworth_lpf_15_n1_mask");
+  saveImage(fox_b_lpf_15_n1_sp, "../result_img/1.b/", "butterworth_lpf_15_n1_sp_filtered");
+  saveImage(fox_b_idft_15_n1_img, "../result_img/1.b/", "butterworth_lpf_15_n1_idft");
 
+  // Butterworth LPF D0 = 15 n = 2
+  cv::Mat fox_b_lpf_15_n2(fox_dft_i.rows, fox_dft_i.cols, CV_32FC2);
+  cv::Mat blpf_15_n2 =  fdf::ButterworthLpf(fox_dft.getComplex(), 15, 2);
+  fdf::applyFilter(fox_dft_i, blpf_15_n2, fox_b_lpf_15_n2);
+  cv::Mat fox_b_lpf_15_n2_sp = CvDft2d::getSpectrumImg(fox_b_lpf_15_n2);
+  CvIDft2d fox_b_idft_15_n2(fox_b_lpf_15_n2);
+  cv::Mat fox_b_idft_15_n2_img =  fox_b_idft_15_n2.getInvImg();
+  cv::normalize(blpf_15_n2, blpf_15_n2, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  saveImage(blpf_15_n2, "../result_img/1.b/", "butterworth_lpf_15_n2_mask");
+  saveImage(fox_b_lpf_15_n2_sp, "../result_img/1.b/", "butterworth_lpf_15_n2_sp_filtered");
+  saveImage(fox_b_idft_15_n2_img, "../result_img/1.b/", "butterworth_lpf_15_n2_idft");
+
+  // Butterworth LPF D0 = 15 n = 5
+  cv::Mat fox_b_lpf_15_n5(fox_dft_i.rows, fox_dft_i.cols, CV_32FC2);
+  cv::Mat blpf_15_n5 =  fdf::ButterworthLpf(fox_dft.getComplex(), 15, 5);
+  fdf::applyFilter(fox_dft_i, blpf_15_n5, fox_b_lpf_15_n5);
+  cv::Mat fox_b_lpf_15_n5_sp = CvDft2d::getSpectrumImg(fox_b_lpf_15_n5);
+  CvIDft2d fox_b_idft_15_n5(fox_b_lpf_15_n5);
+  cv::Mat fox_b_idft_15_n5_img =  fox_b_idft_15_n5.getInvImg();
+  cv::normalize(blpf_15_n5, blpf_15_n5, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  saveImage(blpf_15_n5, "../result_img/1.b/", "butterworth_lpf_15_n5_mask");
+  saveImage(fox_b_lpf_15_n5_sp, "../result_img/1.b/", "butterworth_lpf_15_n5_sp_filtered");
+  saveImage(fox_b_idft_15_n5_img, "../result_img/1.b/", "butterworth_lpf_15_n5_idft");
+
+    // Butterworth LPF D0 = 50 n = 1
+  cv::Mat fox_b_lpf_50_n1(fox_dft_i.rows, fox_dft_i.cols, CV_32FC2);
+  cv::Mat blpf_50_n1 =  fdf::ButterworthLpf(fox_dft.getComplex(), 50, 1);
+  fdf::applyFilter(fox_dft_i, blpf_50_n1, fox_b_lpf_50_n1);
+  cv::Mat fox_b_lpf_50_n1_sp = CvDft2d::getSpectrumImg(fox_b_lpf_50_n1);
+  CvIDft2d fox_b_idft_50_n1(fox_b_lpf_50_n1);
+  cv::Mat fox_b_idft_50_n1_img =  fox_b_idft_50_n1.getInvImg();
+  cv::normalize(blpf_50_n1, blpf_50_n1, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  saveImage(blpf_50_n1, "../result_img/1.b/", "butterworth_lpf_50_n1_mask");
+  saveImage(fox_b_lpf_50_n1_sp, "../result_img/1.b/", "butterworth_lpf_50_n1_sp_filtered");
+  saveImage(fox_b_idft_50_n1_img, "../result_img/1.b/", "butterworth_lpf_50_n1_idft");
+
+  // Butterworth LPF D0 = 50 n = 2
+  cv::Mat fox_b_lpf_50_n2(fox_dft_i.rows, fox_dft_i.cols, CV_32FC2);
+  cv::Mat blpf_50_n2 =  fdf::ButterworthLpf(fox_dft.getComplex(), 50, 2);
+  fdf::applyFilter(fox_dft_i, blpf_50_n2, fox_b_lpf_50_n2);
+  cv::Mat fox_b_lpf_50_n2_sp = CvDft2d::getSpectrumImg(fox_b_lpf_50_n2);
+  CvIDft2d fox_b_idft_50_n2(fox_b_lpf_50_n2);
+  cv::Mat fox_b_idft_50_n2_img =  fox_b_idft_50_n2.getInvImg();
+  cv::normalize(blpf_50_n2, blpf_50_n2, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  saveImage(blpf_50_n2, "../result_img/1.b/", "butterworth_lpf_50_n2_mask");
+  saveImage(fox_b_lpf_50_n2_sp, "../result_img/1.b/", "butterworth_lpf_50_n2_sp_filtered");
+  saveImage(fox_b_idft_50_n2_img, "../result_img/1.b/", "butterworth_lpf_50_n2_idft");
+
+  // Butterworth LPF D0 = 50 n = 5
+  cv::Mat fox_b_lpf_50_n5(fox_dft_i.rows, fox_dft_i.cols, CV_32FC2);
+  cv::Mat blpf_50_n5 =  fdf::ButterworthLpf(fox_dft.getComplex(), 50, 5);
+  fdf::applyFilter(fox_dft_i, blpf_50_n5, fox_b_lpf_50_n5);
+  cv::Mat fox_b_lpf_50_n5_sp = CvDft2d::getSpectrumImg(fox_b_lpf_50_n5);
+  CvIDft2d fox_b_idft_50_n5(fox_b_lpf_50_n5);
+  cv::Mat fox_b_idft_50_n5_img =  fox_b_idft_50_n5.getInvImg();
+  cv::normalize(blpf_50_n5, blpf_50_n5, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+  saveImage(blpf_50_n5, "../result_img/1.b/", "butterworth_lpf_50_n5_mask");
+  saveImage(fox_b_lpf_50_n5_sp, "../result_img/1.b/", "butterworth_lpf_50_n5_sp_filtered");
+  saveImage(fox_b_idft_50_n5_img, "../result_img/1.b/", "butterworth_lpf_50_n5_idft");
+
+  showImage("mask", blpf_50_n5);
+  showImage("filtered sp", fox_b_lpf_50_n5_sp);
+  showImage("idft", fox_b_idft_50_n5_img);
   cv::waitKey(0);
   return 0;
 }
